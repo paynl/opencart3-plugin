6c5091f2d37031a4415fd7eed11a229bc42c9bd4
Jorn Bakker <jorn@pay.nl>
2018-06-22 15:00:01 +0200

    Added paymentMethods Cashly, SprayPay, Wechat Pay

---
+++ upload/admin/controller/extension/payment/paynl_wechatpay.php
@@ -0,0 +1,13 @@
+<?php
+$dir = dirname(dirname(dirname(dirname(dirname(__FILE__)))));
+$autoload = $dir.'/Pay/Autoload.php';
+
+require_once $autoload;
+class ControllerExtensionPaymentPaynlWechatpay extends Pay_Controller_Admin {
+    protected $_paymentOptionId = 1978;
+    protected $_paymentMethodName = 'paynl_wechatpay';
+    
+    protected $_defaultLabel = 'Wechat Pay';
+    
+    
+}
