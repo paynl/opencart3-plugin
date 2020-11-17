762b686e30180e49398d9fdf0acacb139fcbb4e6
woutse <wouter@pay.nl>
2020-11-17 09:55:47 +0100

    Updated versions

--- upload/admin/language/nl-nl/extension/payment/paynl3.php
+++ upload/admin/language/nl-nl/extension/payment/paynl3.php
@@ -41,7 +41,7 @@ $_['entry_sort_order']   = 'Sort Order';
 
 $_['text_extension'] = 'PAY.';
 
-$version = '1.2.6';
+$version = '1.2.7';
 $css = 'position: relative;top:0px;display: inline;left: 10px;';
 $paynl_logo = '<a href="https://www.pay.nl" target="paynl"><img src="https://static.pay.nl/generic/images/50x50/logo.png" alt="PAY." title="PAY." /></a>' .
     '<div style="' . $css . '">Version: ' . $version . '</div>';;
