0196fa386ec8e10a02298293526cd825c0b1829a
Wouter Jonker <wouter@pay.nl>
2021-11-18 10:04:52 +0100

    Update paynl3.php

--- upload/catalog/language/en-gb/extension/payment/paynl3.php
+++ upload/catalog/language/en-gb/extension/payment/paynl3.php
@@ -1,6 +1,6 @@
 <?php
 $_['text_choose_bank'] = 'Choose your bank';
 $_['text_dob'] = 'Date of birth';
-$_['error_dob'] = 'A date of birth is required to finish this transaction, please fill it in.';
+$_['error_dob'] = 'A date of birth is required to finish this transaction, please enter your date of birth.';
 $_['text_cancel'] = 'The payment has been canceled, please try again.';
-$_['text_denied'] = 'Unfortunatly the payment has been denied. Please try again or use another payment method.';
+$_['text_denied'] = 'Unfortunately the payment has been denied. Please try again or use another payment method.';
