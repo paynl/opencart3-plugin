48bd263a1956fef1438ebc6049c678c7ec59d7d7
Anne <anne@pay.nl>
2022-07-26 13:33:22 +0200

    add Nexi payment method

---
+++ upload/catalog/controller/extension/payment/paynl_nexi.php
@@ -0,0 +1,11 @@
+<?php
+$dir = dirname(dirname(dirname(dirname(dirname(__FILE__)))));
+$autoload = $dir . '/Pay/Autoload.php';
+
+require_once $autoload;
+
+class ControllerExtensionPaymentPaynlNexi extends Pay_Controller_Payment
+{
+    protected $_paymentOptionId = 1945;
+    protected $_paymentMethodName = 'paynl_nexi';
+}
