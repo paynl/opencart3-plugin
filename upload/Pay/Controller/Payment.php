1f68a333aa59a8fefd1f43f702784f429fdee854
woutse <wouter@pay.nl>
2023-12-04 11:02:48 +0100

    Bumped versions

--- upload/Pay/Controller/Payment.php
+++ upload/Pay/Controller/Payment.php
@@ -119,7 +119,7 @@ class Pay_Controller_Payment extends Controller
 
             $apiStart->setDescription($description);
             $apiStart->setExtra1($order_info['order_id']);
-            $apiStart->setObject('opencart3 1.8.0');
+            $apiStart->setObject('opencart3 1.8.1');
 
 
             // Klantdata verzamelen en meesturen
