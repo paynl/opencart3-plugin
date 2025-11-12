5331d0282c90e838f1c33f9c0b3e2e52a3b786df
Kevin Verschoor <k.verschoor@pay.nl>
2025-11-12 10:47:30 +0100

    PLUG-4893 - Fix follow payment

--- upload/Pay/Model.php
+++ upload/Pay/Model.php
@@ -467,6 +467,13 @@ class Pay_Model extends Model
             $order_info['payment_method'] = $newPaymentMethod;
 
             if (!empty($order_info['customer_group_id'])) {
+                $order_info['products'] = $this->model_checkout_order->getOrderProducts($orderId);
+                foreach ($order_info['products'] as &$product) {
+                    $product['order_option'] = $this->model_checkout_order->getOrderOptions($orderId, $product['order_product_id']);
+                }        
+                $order_info['totals'] = $this->model_checkout_order->getOrderTotals($orderId);
+                $order_info['vouchers'] = $this->model_checkout_order->getOrderVouchers($orderId);
+
                 $this->model_checkout_order->editOrder($orderId, $order_info);
                 $this->log('addOrderHistory: ' . print_r(array($order_info['order_id'], $orderStatusId, $followPaymentMessage), true));
                 $this->model_checkout_order->addOrderHistory($order_info['order_id'], $orderStatusId, $followPaymentMessage, false);
